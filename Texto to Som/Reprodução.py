980240c2546e7d8d1f36555b466419aaa535d84e
José Ferreira <62623689+kvojps@users.noreply.github.com>
2020-10-23 21:10:38 -0300

    Add files via upload

---
+++ Texto to Som/Reprodução.py
@@ -0,0 +1,9 @@
+import pyttsx3
+#iniciando o módulo
+Texto = pyttsx3.init()
+
+# .say() é usada para reproduzir em forma de som o texto mencionado.
+Texto.say("Não acredito que você viu o último storie e não curtiu e compartilhou o post")
+
+# Processa e roda o programa
+Texto.runAndWait()
