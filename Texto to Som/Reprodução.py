e18a7bd5766b5c4710c1a43f2e191c2990cb781f
José Ferreira <62623689+kvojps@users.noreply.github.com>
2020-10-23 21:13:10 -0300

    Update Reprodução.py

--- Texto to Som/Reprodução.py
+++ Texto to Som/Reprodução.py
@@ -3,7 +3,7 @@ import pyttsx3
 Texto = pyttsx3.init()
 
 # .say() é usada para reproduzir em forma de som o texto mencionado.
-Texto.say("Não acredito que você viu o último storie e não curtiu e compartilhou o post")
+Texto.say("Qualquer coisa")
 
 # Processa e roda o programa
 Texto.runAndWait()
